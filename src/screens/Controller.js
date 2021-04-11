ea732d73273587f519d3d12caf5207d960021763
Shivam Runthala <runthalashivam@gmail.com>
2021-04-11 17:24:54 +0530

    Adding profile files

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,23 +1,23 @@
-import React,{Component} from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Details from './details/Details';
+import Profile from './profile/Profile';
 
-
-
-class Controller extends Component{
-    constructor(){
+class Controller extends Component {
+    constructor() {
         super()
         this.baseUrl = "http://localhost:8080/api/"
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <Router>
-                <div className = 'main-conatiner'>
-                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/>
+                <div className='main-conatiner'>
+                    <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
                     <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
                     <Route path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
 
